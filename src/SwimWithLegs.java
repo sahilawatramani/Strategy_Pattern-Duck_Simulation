c24d13a7d36e48769f2868a2165123364b092500
sahilawatramani <sahilawatramani@gmail.com>
2025-03-06 23:14:24 +0530

    Implemented SwimWithLegs class for ducks that swim normally

---
+++ src/SwimWithLegs.java
@@ -0,0 +1,7 @@
+// Implements the SwimBehaviour interface, representing ducks that swim with legs
+public class SwimWithLegs implements SwimBehaviour{
+    @Override
+    public void swim() {
+        System.out.println("I believe I can Swim...");
+    }
+}
