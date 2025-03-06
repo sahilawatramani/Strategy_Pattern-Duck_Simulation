68eed26cd8ae3f483cedae204c54dec4960d95ca
sahilawatramani <sahilawatramani@gmail.com>
2025-03-06 23:15:12 +0530

    Implemented Floating class for ducks that can only float

---
+++ src/Floating.java
@@ -0,0 +1,7 @@
+// Implements the SwimBehaviour interface, representing ducks that float
+public class Floating implements SwimBehaviour{
+    @Override
+    public void swim() {
+        System.out.println("I believe I can Float");
+    }
+}
